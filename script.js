675b9483d505e81d0b81a5223bc1f894d08813ca
Pashenko Tetyana <tpashen22@gmail.com>
2022-06-29 20:55:56 +0300

    https://github.com/tpashen/user_git_VSC.git

--- script.js
+++ script.js
@@ -2,5 +2,13 @@ alert('Hello!')
 name=prompt('What is your name?')
 alert('Welcome to my site, '+name)
 
+password=prompt('Enter password')
+if (password=='123'){
+    alert(' Password is correct')
+}
+else{
+    alert('Password is incorrect!')
+    window.close()  
+}
 
 
